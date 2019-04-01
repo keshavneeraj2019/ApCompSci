9cc5929177650944cffe634e8bd0fabc5ae7f879
keshavneeraj2019 <42677916+keshavneeraj2019@users.noreply.github.com>
2019-04-01 08:06:50 -0700

    Create main.java

---
+++ Cash Reigster/main.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<Product> inventory = new ArrayList<Product>();
+        inventory.add(new Product("Hamberder",12345,4.20,"Food",5,true));
+
+
+
+
+
+    }
+}
